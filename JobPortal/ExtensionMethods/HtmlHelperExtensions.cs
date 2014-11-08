477033e8674c27a81bbe5ed03d6529778ab836ab
muhammad-azeem-confiz <muhammad.azeem@confiz.com>
2014-11-09 00:25:59 +0500

    Searchbar and banner issue resolved

---
+++ JobPortal/ExtensionMethods/HtmlHelperExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobPortal.ExtensionMethods
+{
+    public static class HtmlHelperExtensions
+    {
+        public static bool IsCurrentControllerAndAction(string controllerName, string actionName, ViewContext viewContext)
+        {
+            bool result = false;
+            string normalizedControllerName = controllerName.EndsWith("Controller") ? controllerName : String.Format("{0}Controller", controllerName);
+
+            if (viewContext == null) return false;
+            if (String.IsNullOrEmpty(actionName)) return false;
+
+            if (viewContext.Controller.GetType().Name.Equals(normalizedControllerName, StringComparison.InvariantCultureIgnoreCase) &&
+                viewContext.Controller.ValueProvider.GetValue("action").AttemptedValue.Equals(actionName, StringComparison.InvariantCultureIgnoreCase))
+            {
+                result = true;
+            }
+
+            return result;
+        }
+    }
+}
