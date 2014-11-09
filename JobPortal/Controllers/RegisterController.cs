da23e1d4515684abc13c002840a80cdf4678220b
Talmeez <mirzatalmeez@hotmail.com>
2014-11-10 01:25:02 +0500

    Pages Define.

---
+++ JobPortal/Controllers/RegisterController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobPortal.Controllers
+{
+    public class RegisterController : Controller
+    {
+        //
+        // GET: /Register/
+        [AllowAnonymous]
+        public ActionResult Employee()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public ActionResult Recruiter()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public ActionResult EmployeeAcademic()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public ActionResult EmployeeProfessional()
+        {
+            return View();
+        }
+	}
+}
